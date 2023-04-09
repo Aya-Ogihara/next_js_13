62cbeef8ba2da05107d420783c42535f85817c5f
AyaOgihara <90658772+Aya-Ogihara@users.noreply.github.com>
2023-04-10 10:04:57 +1200

    ✨ Add Header component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Poppins } from 'next/font/google';
+import Header from './components/header';
 import './globals.css';
 
 const poppins = Poppins({
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={poppins.className}>
+        <Header />
         <main className='container'>{children}</main>
       </body>
     </html>
